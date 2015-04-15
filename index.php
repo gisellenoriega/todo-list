8324a20404dedceeb7bc705bf70075d973cac224
Giselle Noriega <gnoriega626@gmail.com>
2015-04-15 08:15:55 -0700

    todo list video 2

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-indexgg
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple To-Do List</title>
+	<link rel="stylesheet" type="text/css" href="css">
+</head>
+<body>
+	<div class="wrap">
+		
+	</div>
+</body>
+</html>

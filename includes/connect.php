99bda24f76bc4be58b9cf09a86cd7d9dfeab5c15
Giselle Noriega <gnoriega626@gmail.com>
2015-04-17 19:42:19 -0700

    milestone 19

--- includes/connect.php
+++ includes/connect.php
@@ -1,14 +1,12 @@
-<?php
-//  why is this important? why does localhost have to be first?
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
-//  mysqli->connect_error then we want it to die and have this message
-if($mysqli->connect_error) {
-	die('Connect Error (' . $mysqli->connect_errno .')'
+<?php 
+
+$mysqli = new mysqli('localhost' , 'root' , 'root' , 'todo');
+if ($mysqli->connect_error) {
+	die('Connect Error(' . $mysqli->connect_errno . ')' 
 	. $mysqli->connect_error);
-}
-else {
-	// echo "Connection made";
+}else{
+	//echo "Connection Made";
+
 }
 $mysqli->close();
-
 ?>
--- includes/delete-task.php
+++ includes/delete-task.php
@@ -1,9 +1,10 @@
-<?php 
-	$task_id = strip_tags($_POST['id']);
-	require('connect.php');
-	$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+<?php  
+$task_id = strip_tags($_POST['id']);
+require('connect.php');
+$mysqli = new mysqli('localhost' , 'root' , 'root' , 'todo');
 
-	if($result = $mysqli->query("DELETE FROM tasks WHERE id="task_id)) {
+if ($result = $mysqli->query("DELETE FROM tasks WHERE id='task_id'")) {
+	
+  }
 
-	}
 ?>
